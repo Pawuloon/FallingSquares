abedb781277b99317bc1d0104a80fa69b126b2d8
Pawuloon <94827730+Pawuloon@users.noreply.github.com>
2023-06-29 15:49:52 +0200

    Add files via upload

---
+++ GuiPro2/src/Main.java
@@ -0,0 +1,12 @@
+
+
+import javax.swing.*;
+import java.util.HashMap;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(GameFrame::new);
+    }
+}
